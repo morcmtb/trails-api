82a325d247412ecf05052d9d226dad60a3b83b04
aronbraggans <aron.braggans@cargill.com>
2018-03-04 11:34:13 -0600

    Sorting Trails by Name

--- service/list.js
+++ service/list.js
@@ -1,14 +1,17 @@
-import * as dynamoDbLib from "../libs/dynamodb-lib";
-import { success, failure } from "../libs/response-lib";
+import * as dynamoDbLib from "../libs/dynamodb-lib"
+import { success, failure } from "../libs/response-lib"
 
 export async function main(event, context, callback) {
-  const params = {
-    TableName: "roam-trails-api"
-  };
-  try {
-    const result = await dynamoDbLib.call("scan", params);
-    callback(null, success(result.Items));
-  } catch (e) {
-    callback(null, failure({ status: false, error: e.message }));
-  }
+    const params = {
+        TableName: "roam-trails-api"
+    }
+    try {
+        const result = await dynamoDbLib.call("scan", params)
+        result.Items.sort((a, b) => {
+            return a.trailName > b.trailName ? 1 : -1
+        })
+        callback(null, success(result.Items))
+    } catch (e) {
+        callback(null, failure({ status: false, error: e.message }))
+    }
 }
