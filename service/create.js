cba6968a0baa86a797231bd323f7dcabdc969960
Aron <aron.braggans@gmail.com>
2020-01-25 07:36:32 -0600

    now that is done, lets break it again

--- service/create.js
+++ service/create.js
@@ -1,6 +1,6 @@
 import uuid from "uuid";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
