4cc2187c35a10541577a3dbae1bf67fdb3c8cd1e
Aron Braggans <aron.braggans@gmail.com>
2020-01-26 14:03:49 -0600

    Master (#4)
    * updating packages
    
    * adding @babel/core
    
    * updating packages
    
    * back
    
    * missing SDM
    
    * Update README.md
    
    * testing new babel setup
    
    * preset
    
    * removed preset
    
    * sdm
    
    * service path
    
    * path changes
    
    * more testing
    
    * adding service path back
    
    * yip
    
    * testing path
    
    * path
    
    * moving functions to root of the project
    
    * testing
    
    * test
    
    * hrm
    
    * semi
    
    * fixing lint errors
    
    * now that is done, lets break it again
    
    * pathing
    
    * trying old config
    
    * pathing
    
    * adding serverless bundle
    
    * changing region && policy
    
    * taking out domain work
    
    * missed the plugin
    
    * updates for db
    
    * missed stage
    
    * adding domain back
    
    * fixed yml
    
    * fixing config
    
    * adding custtomDomain to custom block
    
    * sls create
    
    * fixing yml
    
    * certificateName
    
    * test
    
    * changed back to wildcard

--- service/delete.js
+++ service/delete.js
@@ -1,18 +1,18 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib'
-import { success, failure } from '../libs/response-lib'
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
 
 export async function main(event, context) {
   const params = {
-    TableName: 'roam-trails-api',
+    TableName: process.env.tableName,
     Key: {
       trailId: event.pathParameters.id
     }
-  }
+  };
 
   try {
-    const result = await dynamoDbLib.call('delete', params)
-    return success({ status: true })
+    const result = await dynamoDbLib.call("delete", params);
+    return success({ status: true, result: result });
   } catch (e) {
-    return failure({ status: false, error: e.message })
+    return failure({ status: false, error: e.message });
   }
 }
