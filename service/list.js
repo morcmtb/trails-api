8b73d77fcedacf1c2a5c106dbab27e255f1b6d77
Aron <aron.braggans@gmail.com>
2020-01-26 20:38:54 -0600

    test

--- service/list.js
+++ service/list.js
@@ -3,7 +3,7 @@ import { success, failure } from "../libs/response-lib";
 
 export async function main(event, context) {
   const params = {
-    TableName: process.env.tableName
+    TableName: `${process.env.tableName}`
   };
   try {
     const result = await dynamoDbLib.call("scan", params);
