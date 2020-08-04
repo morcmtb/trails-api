a1d0c468f9b89d89343a6345b4137a3c6b0bd74b
Aron <aron.braggans@gmail.com>
2020-08-04 16:48:19 -0500

    wip

--- users-service/service/handler.js
+++ users-service/service/handler.js
@@ -1,18 +1,32 @@
-import * as CognitoLib from '../../libs/cognito-lib';
-import { success, failure } from '../../libs/response-lib';
+import * as CognitoLib from "../../libs/cognito-lib";
+import { success, failure } from "../../libs/response-lib";
 
 export async function hello(event, context) {
   const params = {
-    UserPoolId: '',
-    AttributesToGet: ['username', 'email'],
-    Filter: '',
-    Limit: 0,
+    UserPoolId: "",
+    AttributesToGet: null,
+    Filter: "",
+    Limit: 10,
   };
 
   try {
-    const results = await CognitoLib.call('listUsers', params);
+    const results = await CognitoLib.call("listUsers", params);
+    const { Users } = results;
 
-    return success({ message: results });
+    const usersList = [];
+
+    for (let index = 0; index < Users.length; index++) {
+      const user = Users[index];
+      const { Username } = user;
+      const groups = await forUserGetGroup(Username);
+
+      usersList.push({
+        ...user,
+        groups: groups,
+      });
+    }
+
+    return success(usersList);
   } catch (e) {
     return failure({
       status: false,
@@ -20,3 +34,22 @@ export async function hello(event, context) {
     });
   }
 }
+
+const forUserGetGroup = async (userName) => {
+  const params = {
+    UserPoolId: "",
+    Username: userName,
+  };
+
+  try {
+    const results = await CognitoLib.call("adminListGroupsForUser", params);
+    const { Groups } = results;
+
+    return Groups;
+  } catch (e) {
+    return failure({
+      status: false,
+      error: e,
+    });
+  }
+};
